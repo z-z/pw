d53754beb3abaa9659ab81092686c28fa688bcf9
Artem Dragunov <dragunovartem99@gmail.com>
2023-04-14 22:16:10 +0300

    Restore autocomplete hints

--- pass-completions.sh
+++ pass-completions.sh
@@ -1,13 +1,13 @@
 #/usr/bin/env bash
 _pass_completions()
 {
-  if [ "${#COMP_WORDS[@]}" -lt 2 ] ; then
-    COMPREPLY=($(compgen -W "list get add del ssh help" "${COMP_WORDS[1]}"))
-  elif [[ "${COMP_WORDS[1]}" == "get" || "${COMP_WORDS[1]}" == "del" ]] ; then
-    COMPREPLY=($(compgen -W "$(cat $PW_PATH/pw.txt | awk -F ':::::' 'BEGIN { ORS=" " }; {print $1}')" -- "${COMP_WORDS[2]}"))
-  else
-    COMPREPLY=()
-  fi
+	if [[ "${COMP_WORDS[1]}" == "-" ]] ; then
+		COMPREPLY=($(compgen -W "-add -help -ls -rm -ssh" "${COMP_WORDS[1]}"))
+	elif [[ "${#COMP_WORDS[@]}" -lt 2 ||
+			"${COMP_WORDS[1]}" == "-ssh" ||
+			"${COMP_WORDS[1]}" == "-rm" ]] ; then
+		COMPREPLY=($(compgen -W "$(cat $PW_PATH/pw.txt | awk -F ':::::' 'BEGIN { ORS=" " }; {print $1}')" -- "${COMP_WORDS[2]}"))
+	fi
 }
 
 complete -F _pass_completions pw
