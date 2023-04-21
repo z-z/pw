b57c3d62ce1fe0ee9fd8a985995a5f4b4541fc30
Artem Dragunov <dragunovartem99@gmail.com>
2023-04-21 08:25:32 +0300

    Separate short and long options

--- pass-completions.sh
+++ pass-completions.sh
@@ -2,10 +2,14 @@
 _pass_completions()
 {
 	if [[ "${COMP_WORDS[1]}" == "-" ]] ; then
-		COMPREPLY=($(compgen -W "-add -help -ls -rm -ssh" "${COMP_WORDS[1]}"))
+		COMPREPLY=($(compgen -W "-a -h -l -r -s" "${COMP_WORDS[1]}"))
+	elif [[ "${COMP_WORDS[1]}" == "--" ]] ; then
+		COMPREPLY=($(compgen -W "--add --help --list --remove --ssh" "${COMP_WORDS[1]}"))
 	elif [[ "${#COMP_WORDS[@]}" -lt 2 ||
-			"${COMP_WORDS[1]}" == "-ssh" ||
-			"${COMP_WORDS[1]}" == "-rm" ]] ; then
+			"${COMP_WORDS[1]}" == "-s" ||
+			"${COMP_WORDS[1]}" == "-r" ||
+			"${COMP_WORDS[1]}" == "--ssh" ||
+			"${COMP_WORDS[1]}" == "--remove" ]] ; then
 		COMPREPLY=($(compgen -W "$(cat $PW_PATH/pw.txt | awk -F ':::::' 'BEGIN { ORS=" " }; {print $1}')" -- "${COMP_WORDS[2]}"))
 	fi
 }
